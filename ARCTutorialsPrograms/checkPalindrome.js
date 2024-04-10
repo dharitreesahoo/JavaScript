5b4b96e72dc5cece21c533644707257a62a232ff
Dharitree Sahoo <dharitreesahoo1@gmail.com>
2024-04-10 10:42:14 +0100

    added JS files

--- ARCTutorialsPrograms/checkPalindrome.js
+++ ARCTutorialsPrograms/checkPalindrome.js
@@ -1,11 +1,21 @@
-let str = "Madam"
-str = str.toLowerCase();
+// let str = "Madam"
+// str = str.toLowerCase();
 
-const strNew = str.split('').reverse().join('')
-console.log(strNew);
-if(str==strNew)
+// const strNew = str.split('').reverse().join('')
+// console.log(strNew);
+// if(str==strNew)
+// {
+//     console.log("Palindrome");
+// }else{
+//     console.log("not palindrome");
+// }
+
+let str ="Madam"
+const strRev = str.split("").reverse().join("")
+if(strRev==str)
 {
     console.log("Palindrome");
 }else{
     console.log("not palindrome");
 }
+
--- ArrayNew/arrMapObj.js
+++ ArrayNew/arrMapObj.js
@@ -2,8 +2,19 @@ let arr = [{name:"jini", roll:12},
 {name:"mama",roll:23},
 {name:"mama",roll:23}]
 
-const arr1 = Array.from(new Set(arr.map((ele)=>
-{
-   return  ele.roll
-})));
+// const arr1 = Array.from(new Set(arr.map((ele)=>
+// {
+//    return  ele.roll
+// })));
+// console.log(arr1);
+
+
+// const arr1 = Array.from(new Set(arr.map((ele)=>{
+//    return ele.roll
+// })));
+const arr1 = arr.map((ele)=>{
+   return ele.roll
+}).filter((ele)=>{
+   return ele.name=="mama"
+})
 console.log(arr1);
